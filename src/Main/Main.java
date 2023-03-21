d3f77eb65b6d97d490081f71a881653325d48be6
Brandon Kmiec <brandon.m.kmiec@gmail.com>
2023-03-21 13:42:09 -0700

    Checkpoint #3

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,7 +4,6 @@ import java.awt.*;
 import java.io.File;
 
 import Data.Animation;
-import Data.Click;
 import Data.Frame;
 import Data.RECT;
 import FileIO.EZFileWrite;
@@ -15,13 +14,10 @@ public class Main {
     // Fields (Static) below...
     public static String s = "";
     public static RECT r;
+    public static RECT chicken;
     public static final int dropShadow = 2;
     private static Animation anim;
 
-//    //Checkpoint #2
-//    public static String str = "";
-//    public static RECT[] rs;
-    // End Static fields...
 
     public static void main(String[] args) {
         updateArtScript();
@@ -32,55 +28,40 @@ public class Main {
     /* This is your access to things BEFORE the game loop starts */
     public static void start(Control ctrl) {
         // TODO: Code your starting conditions here...NOT DRAW CALLS HERE! (no addSprite or drawString)
-//        r = new RECT(46, 3, 107, 123, "Persephone", new Frame(0, 0, "f1"));
-        anim = new Animation(120, true);
-        anim.addFrame(new Frame(0, 0, "c_idle"));
-        anim.addFrame(new Frame(0, 0, "c_leftfoot"));
-        anim.addFrame(new Frame(0, 0, "c_idle"));
-        anim.addFrame(new Frame(0, 0, "c_rightfoot"));
+        r = new RECT(46, 3, 107, 123, "Persephone", new Frame(0, 0, "f1"));
+        chicken = new RECT(1792, 0, 1910, 128, "Chicken", "Chicken");
 
-//        //Checkpoint #2
-//        rs = new RECT[3];
-//        rs[0] = new RECT(166, 248, 261, 372, "Tree");
-//        rs[1] = new RECT(546, 404, 618, 538, "Persephone");
-//        rs[2] = new RECT(803, 358, 923, 462, "Cheese Burger");
+        anim = new Animation(128, true);
+        anim.addFrame(new Frame(1792, 0, "c_idle"));
+        anim.addFrame(new Frame(1792, 0, "c_leftfoot"));
+        anim.addFrame(new Frame(1792, 0, "c_idle2"));
+        anim.addFrame(new Frame(1792, 0, "c_rightfoot"));
     }
 
     /* This is your access to the "game loop" (It is a "callback" method from the Control class (do NOT modify that class!))*/
     public static void update(Control ctrl) {
         // TODO: This is where you can code! (Starting code below is just to show you how it works)
+        ctrl.addSpriteToFrontBuffer(0, 0, "f0");
+
         Frame curFrame = anim.getCurrentFrame();
         if (curFrame != null)
             ctrl.addSpriteToFrontBuffer(curFrame);
-//        ctrl.addSpriteToFrontBuffer(0, 0, "f0");
-//        Point p = Mouse.getMouseCoords();
-//        int x = (int) p.getX();
-//        int y = (int) p.getY();
-//        if (r.isCollision(x, y))
-//            ctrl.addSpriteToFrontBuffer(r.getGraphicalHover().getX(), r.getGraphicalHover().getY(),
-//                    r.getGraphicalHover().getSpriteTag());
-        //s = r.getHoverLabel();
-        //else
-        //s = "";
-//        ctrl.drawString(x, (y - 2), s, Color.BLACK);
-//        ctrl.drawString(x - dropShadow, ((y - dropShadow) - 2), s, Color.yellow);
 
-//        //Checkpoint #2
-//        ctrl.addSpriteToFrontBuffer(150, 250, "tree");
-//        ctrl.addSpriteToFrontBuffer(500, 400, "f0");
-//        ctrl.addSpriteToFrontBuffer(800, 350, "cheeseBurger");
-//
-//        if (Control.getMouseInput() != null) {
-//            for (RECT r : rs) {
-//                if (r.isClicked(Control.getMouseInput(), Click.LEFT_BUTTON)) {
-//                    str = r.getTag() + " was Clicked.";
-//                    break;
-//                }
-//                else
-//                    str = "";
-//            }
-//        }
-//        ctrl.drawString(800, 540, str, Color.white);     // Test Only
+        Point p = Mouse.getMouseCoords();
+        int x = (int) p.getX();
+        int y = (int) p.getY();
+
+        if (r.isCollision(x, y)) {
+            ctrl.addSpriteToFrontBuffer(r.getGraphicalHover().getX(), r.getGraphicalHover().getY(),
+                    r.getGraphicalHover().getSpriteTag());
+        }
+        if (chicken.isCollision(x, y))
+            s = chicken.getHoverLabel();
+        else
+            s = "";
+
+        ctrl.drawString(x, (y - 2), s, Color.BLACK);
+        ctrl.drawString(x - dropShadow, ((y - 2) - dropShadow), s, Color.YELLOW);
     }
 
     // Additional Static methods below...(if needed)
