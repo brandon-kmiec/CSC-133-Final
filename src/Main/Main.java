af81281246bfccf4a496bba08b903a3121f95483
Brandon Kmiec <brandon.m.kmiec@gmail.com>
2023-04-10 19:00:38 -0700

    Checkpoint #5 (Work in Progress)

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -3,25 +3,30 @@ package Main;
 import Data.Click;
 import Data.Command;
 import Data.RECT;
+import Data.Frame;
 import FileIO.EZFileRead;
 import FileIO.EZFileWrite;
 import Input.Mouse;
+import Particles.ParticleSystem;
+import Particles.Rain;
 import ScriptingEngine.Interpreter;
 import logic.Control;
 
 import java.awt.*;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.StringTokenizer;
 
 public class Main {
     // Fields (Static) below...
-    public static String s = "";
-    public static String s2 = "";
-    private static int[] buffer;
-    private static RECT disk;
-    private static final int dropShadow = 2;
-    private static Interpreter interpreter;
+    public static Rain rain;
+//    public static String s = "";
+//    public static String s2 = "";
+//    private static int[] buffer;
+//    private static RECT disk;
+//    private static final int dropShadow = 2;
+//    private static Interpreter interpreter;
 
 
     public static void main(String[] args) {
@@ -33,52 +38,62 @@ public class Main {
     /* This is your access to things BEFORE the game loop starts */
     public static void start(Control ctrl) {
         // TODO: Code your starting conditions here...NOT DRAW CALLS HERE! (no addSprite or drawString)
-        EZFileRead ezr = new EZFileRead("script.txt");
-        ArrayList<Command> commands = new ArrayList<>();
-        for (int i = 0; i < ezr.getNumLines(); i++) {
-            String raw = ezr.getLine(i);
-            raw = raw.trim();
-            if (!raw.equals("")) {
-                boolean b = raw.charAt(0) == '#';
-                if (!b)
-                    commands.add(new Command(raw));
-            }
-        }
+        rain = new Rain(-50, 0, 1200, 90, 25, 60, 150);
 
-        disk = new RECT(101, 52, 162, 112, "savetag", "Save Game");
-        buffer = new int[40];
-        for (int i = 0; i < buffer.length; i++) {
-            int value = (int) (Math.random() * 100);
-            buffer[i] = value;
-        }
-
-        interpreter = new Interpreter(ctrl, commands);
+//        EZFileRead ezr = new EZFileRead("script.txt");
+//        ArrayList<Command> commands = new ArrayList<>();
+//        for (int i = 0; i < ezr.getNumLines(); i++) {
+//            String raw = ezr.getLine(i);
+//            raw = raw.trim();
+//            if (!raw.equals("")) {
+//                boolean b = raw.charAt(0) == '#';
+//                if (!b)
+//                    commands.add(new Command(raw));
+//            }
+//        }
+//
+//        disk = new RECT(101, 52, 162, 112, "savetag", "Save Game");
+//        buffer = new int[40];
+//        for (int i = 0; i < buffer.length; i++) {
+//            int value = (int) (Math.random() * 100);
+//            buffer[i] = value;
+//        }
+//
+//        interpreter = new Interpreter(ctrl, commands);
     }
 
     /* This is your access to the "game loop" (It is a "callback" method from the Control class (do NOT modify that class!))*/
     public static void update(Control ctrl) {
         // TODO: This is where you can code! (Starting code below is just to show you how it works)
-        interpreter.checkCommands();
+        ctrl.addSpriteToFrontBuffer(0, 0, "forest");
+        ParticleSystem pm2 = rain.getParticleSystem();
+        Iterator<Frame> it2 = pm2.getParticles();
+        while (it2.hasNext()){
+            Frame par2 = it2.next();
+            ctrl.addSpriteToFrontBuffer(par2.getX(), par2.getY(), par2.getSpriteTag());
+        }
 
-        Point p = Mouse.getMouseCoords();
-        int x = (int) p.getX();
-        int y = (int) p.getY();
-
-        ctrl.addSpriteToFrontBuffer(100, 50, "saveicon");
-
-        if (disk.isCollision(x, y))
-            s = disk.getHoverLabel();
-        else
-            s = "";
-
-        ctrl.drawString(x, (y - 2), s, Color.BLACK);
-        ctrl.drawString(x - dropShadow, (y - dropShadow) - 2, s, Color.yellow);
-        if (Control.getMouseInput() != null)
-            if (disk.isClicked(Control.getMouseInput(), Click.LEFT_BUTTON)) {
-                saveData();
-                s2 = "Game Saved";
-            }
-        ctrl.drawString(0, 200, s2, Color.WHITE);
+//        interpreter.checkCommands();
+//
+//        Point p = Mouse.getMouseCoords();
+//        int x = (int) p.getX();
+//        int y = (int) p.getY();
+//
+//        ctrl.addSpriteToFrontBuffer(100, 50, "saveicon");
+//
+//        if (disk.isCollision(x, y))
+//            s = disk.getHoverLabel();
+//        else
+//            s = "";
+//
+//        ctrl.drawString(x, (y - 2), s, Color.BLACK);
+//        ctrl.drawString(x - dropShadow, (y - dropShadow) - 2, s, Color.yellow);
+//        if (Control.getMouseInput() != null)
+//            if (disk.isClicked(Control.getMouseInput(), Click.LEFT_BUTTON)) {
+//                saveData();
+//                s2 = "Game Saved";
+//            }
+//        ctrl.drawString(0, 200, s2, Color.WHITE);
     }
 
     // Additional Static methods below...(if needed)
@@ -97,26 +112,26 @@ public class Main {
         updateArt.saveFile();
     }
 
-    public static void saveData() {
-        String out = "";
-        for (int i = 0; i < buffer.length; i++)
-            out += buffer[i] + "*";
-        out = out.substring(0, out.length() - 1);
-        EZFileWrite ezw = new EZFileWrite("save.txt");
-        ezw.writeLine(out);
-        ezw.saveFile();
-    }
-
-    public static void loadData() {
-        EZFileRead ezr = new EZFileRead("save.txt");
-        String raw = ezr.getLine(0);
-        StringTokenizer st = new StringTokenizer(raw, "*");
-        if (st.countTokens() != buffer.length)
-            return;
-        for (int i = 0; i < buffer.length; i++) {
-            String value = st.nextToken();
-            int val = Integer.parseInt(value);
-            buffer[i] = val;
-        }
-    }
+//    public static void saveData() {
+//        String out = "";
+//        for (int i = 0; i < buffer.length; i++)
+//            out += buffer[i] + "*";
+//        out = out.substring(0, out.length() - 1);
+//        EZFileWrite ezw = new EZFileWrite("save.txt");
+//        ezw.writeLine(out);
+//        ezw.saveFile();
+//    }
+//
+//    public static void loadData() {
+//        EZFileRead ezr = new EZFileRead("save.txt");
+//        String raw = ezr.getLine(0);
+//        StringTokenizer st = new StringTokenizer(raw, "*");
+//        if (st.countTokens() != buffer.length)
+//            return;
+//        for (int i = 0; i < buffer.length; i++) {
+//            String value = st.nextToken();
+//            int val = Integer.parseInt(value);
+//            buffer[i] = val;
+//        }
+//    }
 }
---
+++ src/Particles/ParticleSystem.java
@@ -0,0 +1,61 @@
+package Particles;
+
+import Data.Frame;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ParticleSystem {
+    // Fields
+    private Particle[] particles;
+    private int x, y;
+    private int xRange, yRange;
+    private int maxLife;
+    private String[] spriteTags;
+
+    // Constructor
+    public ParticleSystem(int numParticles, int x, int y, int xRange, int yRange, int minLife, int maxLife, int xMove,
+                          int yMove, int minDelay, int maxDelay, String[] spriteTags) {
+        this.xRange = xRange;
+        this.yRange = yRange;
+        this.x = x;
+        this.y = y;
+        this.maxLife = maxLife;
+        particles = new Particle[numParticles];
+        this.spriteTags = spriteTags;
+        initParticles(xMove, yMove, minDelay, maxDelay, minLife);
+    }
+
+    // Methods
+    private void initParticles(int xMove, int yMove, int minDelay, int maxDelay, int minLife) {
+        for (int i = 0; i < particles.length; i++) {
+            int n = spriteTags.length;
+            int index = Particle.getRandomInt(0, n - 1);
+            particles[i] = new Particle(x, (x + xRange), y, (y + yRange), spriteTags[index], minLife, maxLife, xMove,
+                    yMove, minDelay, maxDelay);
+        }
+        boolean isDone = false;
+        while (!isDone) {
+            isDone = true;
+            for (int i = 0; i < particles.length; i++) {
+                particles[i].simulateAge();
+                if (!particles[i].hasBeenReset())
+                    isDone = false;
+            }
+        }
+    }
+
+    public Particle[] getParticleArray() {
+        return particles;
+    }
+
+    public Iterator<Frame> getParticles() {
+        List<Frame> parts = new ArrayList<>();
+        for (int i = 0; i < particles.length; i++) {
+            Frame tmp = particles[i].getCurrentFrame();
+            parts.add(tmp);
+        }
+        return parts.iterator();
+    }
+}
